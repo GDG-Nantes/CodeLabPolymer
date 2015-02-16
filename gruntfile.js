6134538d0662e3d3adbeeaa0b6549767a7329899
jefBinomed <jean.francois.garreau@gmail.com>
2015-02-16 22:09:23 +0100

    passage a grunt

---
+++ gruntfile.js
@@ -0,0 +1,31 @@
+module.exports = function (grunt) {
+
+  // Configuration du build
+  grunt.initConfig({
+
+    // Paramétrage
+    watch:{
+
+    },
+    browserSync: {
+        bsFiles: {
+            src : ['**/*.css',
+                  '**/*.html',
+                  '**/*.js']
+        },
+        options: {
+            server: {
+                baseDir: "./"
+            },
+            watchTask: true 
+        }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-watch');   
+  grunt.loadNpmTasks('grunt-browser-sync');
+  
+  // Déclaration des taches
+  grunt.registerTask('default', ['browserSync','watch']);
+
+};
