4a42ec1ef026ac7cd35d0739ccc6cd6d87271d77
jefBinomed <jean.francois.garreau@gmail.com>
2015-02-16 21:25:57 +0100

    mise en place step0

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+var gulp        = require('gulp');
+var browserSync = require('browser-sync');
+
+// Static server
+gulp.task('browser-sync', function() {
+    browserSync({
+        server: {
+            baseDir: "./"
+        }
+    });
+});
+
+// use default task to launch BrowserSync and watch JS files
+gulp.task('default', ['browser-sync'], function () {
+
+    // add browserSync.reload to the tasks array to make
+    // all browsers reload after tasks are complete.
+    gulp.watch("*/*.js", [browserSync.reload]);
+    gulp.watch("*/*.html", [browserSync.reload]);
+    gulp.watch("*/*.css", [browserSync.reload]);
+});
